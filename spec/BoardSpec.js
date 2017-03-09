d629ee7703252fe7233c69029942933b01befa03
John Ashton <John_ashton@hotmail.com>
2017-03-09 16:15:16 +0000

    Added test for board instantiation as well as passing

--- spec/BoardSpec.js
+++ spec/BoardSpec.js
@@ -1,3 +1,12 @@
 describe("Board", function() {
-  // code
+
+
+    beforeEach(function(){
+      board = new Board();
+    });
+
+    it('Should initialise with a length of 3', function(){
+      expect(board.showBoard().length).toEqual(3)
+    });
+
 });
--- src/Board.js
+++ src/Board.js
@@ -1,1 +1,10 @@
+function Board(square){
+  this._board = [[new Square(),new Square(),new Square()],
+                [new Square(),new Square(),new Square()],
+                [new Square(),new Square(),new Square()]];
 
+};
+
+Board.prototype.showBoard = function(){
+  return this._board;
+};
